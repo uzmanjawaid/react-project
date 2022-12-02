4eb43ddbd69788945f64e728147487d24f0945cb
uzman jawaid <uzman.jawaid.siddiq@gmail.com>
2022-12-02 22:50:18 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+function Booklist() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Title = () => <h1>Healing The Emptiness</h1>;
+const Image = () => {
+  return (
+    <img
+      src='https://www.readings.com.pk/Pages/Categories/BookImages/9789696402275.jpg'
+      alt=''
+    />
+  );
+};
+const Author = () => <h4>Yasir Jawaid</h4>;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Booklist />, document.getElementById('root'));
